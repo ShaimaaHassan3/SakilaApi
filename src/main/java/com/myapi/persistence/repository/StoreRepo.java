52a00b37e5039bcbeb54fceccb41700efc80edfc
ShaimaaHassan3 <70854824+ShaimaaHassan3@users.noreply.github.com>
2023-04-16 13:10:45 +0200

    Handle Store Services

--- src/main/java/com/myapi/persistence/repository/StoreRepo.java
+++ src/main/java/com/myapi/persistence/repository/StoreRepo.java
@@ -1,8 +1,14 @@
 package com.myapi.persistence.repository;
 
+import com.myapi.persistence.entities.Staff;
 import com.myapi.persistence.entities.Store;
+
+import java.util.Set;
 
 public interface StoreRepo {
 
     public Store getStoreById(int ID);
+    public Set<Store> getAllStories();
+    public Staff getManager(int storeId);
+
 }
